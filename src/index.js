818f16c91b4c1f454332f22bd70eef6aa9148eeb
ValentinNM <57604614+ValentinNM@users.noreply.github.com>
2021-08-03 19:44:14 +0300

    likes section solved

--- src/index.js
+++ src/index.js
@@ -3,67 +3,85 @@ console.log("working");
 
 
 let images = [];
+let commentsData = [];
+
 const imageContainrEL = document.querySelector(".image-container");
 
 fetch("http://localhost:3000/images")
   .then((res) => res.json())
   .then((data) => {
-    console.log("Inside db data: ", data);
+    console.log("Inside images data: ", data);
     images = data;
-renderPostCard(images);
-});
-  
 
+    renderPostCard(images);
+});
 
 function renderPostCard (images) { 
-   
+    console.log('images:', images);
+
+    
+    images.forEach(imageData => {
+    console.log("image data; ", imageData);  
     const articleEl = document.createElement ("article");
+    articleEl.className = "image-card";
 
     const imageTitleEl = document.createElement("h2");
     imageTitleEl.className = 'title';
-    imageTitleEl.innerText = "Title of image to be put here";
+    imageTitleEl.innerText = imageData.title;
     
     articleEl.append(imageTitleEl);
 
-    const image = images[0].image;
-    // console.log("image:", image);
     const postImgEl = document.createElement ("img");
     postImgEl.className = "image";
-    postImgEl.src = image;
+    postImgEl.src = imageData.image;
     articleEl.append(postImgEl);
-
-
 
     const likesSectionDivEl = document.createElement("div");
     likesSectionDivEl.className = "likes-section";
     const spanEl = document.createElement("span");
     spanEl.className = "likes";
-    spanEl.innerText = "0 likes";
+    spanEl.innerText = `${imageData.likes} likes`;
     
     const likeButtonEl = document.createElement("button");
     likeButtonEl.className = "like-button";
     likeButtonEl.innerText = " ♥ ";
+
+    likeButtonEl.addEventListener("click", () => {
+        console.log("clicked", imageData.id, imageData.likes);
+    // const previousLikes = imageData.likes ;
+
+    const fetchOptions = {
+        method: "PATCH",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify({likes: imageData.likes + 1})
+    };
+    
+    fetch(`http://localhost:3000/images/${imageData.id}`, fetchOptions)
+        .then((res) => res.json())
+        .then((updatedLikes) => {
+          console.log("Inside PATCH Fetch: ", updatedLikes);
+          spanEl.innerText = `${imageData.likes += 1} likes`;
+
+        });
+      
+    })
     
     likesSectionDivEl.append(spanEl, likeButtonEl);
     articleEl.append(likesSectionDivEl);
-    
-    
-    // for (let i = 1; i < images.length; i++) { 
-        //     console.log("loop data: ", images);
-        //     const pet = images[i];
-        //     const petComments = pet.comments;
-        //     console.log("petComments:", petComments);
         
-        
-        // }
-        
-    const comment = images[0].comments[0].content;
-
     const commentsUlEl = document.createElement("ul");
     commentsUlEl.className = "comments";
-    const commentEl = document.createElement("li");
-    commentEl.innerText = comment;
-    commentsUlEl.append(commentEl);
+
+    imageData.comments.forEach(comment => { 
+        
+        const commentEl = document.createElement("li");
+        commentEl.innerText = comment.content;
+        commentsUlEl.append(commentEl);
+        
+    });
+    
     articleEl.append(commentsUlEl);
 
     const formEl = document.createElement("form");
@@ -85,9 +103,16 @@ function renderPostCard (images) {
     articleEl.append(formEl);
     
     imageContainrEL.append(articleEl);
+
+    })
 }
 
 
+// create function to render all comments
+
+// create function to render all the post containers on the feed
+
+// function the new comments to be added in the comments section
 
 
 
